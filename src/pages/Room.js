a30302c2a6f3d5db4705187eadbfd5e4db4faa34
ikdshn <harohawayu0504@gmail.com>
2020-10-01 13:17:25 +0900

    start

---
+++ src/pages/Room.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+// import Room from "./pages/Room";
+
+const Room = () => {
+  return (
+    <>
+      <h1>Room</h1>
+    </>
+  );
+};
+
+export default Room;
