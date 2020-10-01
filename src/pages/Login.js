1657f72d6c793adee7cf0910547a8ac47c5eb84d
ikdshn <harohawayu0504@gmail.com>
2020-10-01 21:28:26 +0900

    LoggedInRoute

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "../config/firebase";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">E-mail</label>
           <input type="email" id="email" name="email" placeholder="email" />
