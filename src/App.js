0c269c2c910945075166bcc45809f0e37ff1c9ce
ikdshn <harohawayu0504@gmail.com>
2020-10-15 00:10:55 +0900

    material ui 導入

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Room from "./pages/Room";
@@ -9,11 +9,9 @@ import LoggedInRoute from "./LoggedInRoute";
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <LoggedInRoute exact path="/" component={Room} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-      </Switch>
+      <LoggedInRoute exact path="/" component={Room} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
     </Router>
   );
 };
