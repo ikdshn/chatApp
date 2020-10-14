8c199247acb4b9526797841612d33eac4637a00f
ikdshn <harohawayu0504@gmail.com>
2020-10-14 23:52:24 +0900

    intellijのprettierでフォーマット

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Room from "./pages/Room";
-import LoggedInRoute from './LoggedInRoute';
+import LoggedInRoute from "./LoggedInRoute";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <LoggedInRoute exact path="/" component={Room}/>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/signup" component={SignUp}/>
+        <LoggedInRoute exact path="/" component={Room} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
       </Switch>
     </Router>
   );
